1ac9fe05cac72ab5cbf5acb8535b04488b560775
Ethan Nguyen <etnguyen16@gmail.com>
2023-05-30 15:29:21 -0600

    add task model

---
+++ app/models/task.rb
@@ -0,0 +1,11 @@
+class Task < ApplicationRecord
+    validates :name, presence: true
+
+  def self.completed
+    where(completed: true)
+  end
+
+  def self.incomplete
+    where(completed: false)
+  end
+end
