93d8fb933b47d94a7e974663796c610b5862eed7
Ethan Nguyen <etnguyen16@gmail.com>
2023-05-31 23:56:18 -0600

    update routes for clear all button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
+  resources :tasks do 
+    collection do 
+      delete 'clear_all', action: 'clear_all'
+    end
+  end
   root 'tasks#index'
 end
