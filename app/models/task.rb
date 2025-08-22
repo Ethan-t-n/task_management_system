612f4e550bacc740657dddaa82f4e018c318bc8f
Ethan Nguyen <102835975+Ethan-t-n@users.noreply.github.com>
2025-08-22 00:12:19 -0600

    Revamp UI with card grid and gradient navbar

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,7 +11,8 @@ class Task < ApplicationRecord
   end
 
   def time_frame
-    "#{time_frame_start} - #{time_frame_end}" if time_frame_start && time_frame_end
+    return unless time_start && time_end
+    "#{time_start.strftime('%-I:%M%p')} - #{time_end.strftime('%-I:%M%p')}"
   end
 
 end
